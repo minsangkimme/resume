508db01134a49b07526969a17dc16dd1abc080c1
minsangkimme <minsang.tech@gmail.com>
2022-04-23 12:13:40 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactWordcloud, { OptionsProp } from "react-wordcloud";
+
+import "tippy.js/dist/tippy.css";
+import "tippy.js/animations/scale.css";
+
+import words from './data/words';
+
+const options: OptionsProp = {
+  colors: ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b"],
+  enableTooltip: true,
+  deterministic: false,
+  fontFamily: "Nanum Gothic",
+  fontSizes: [12, 90],
+  fontStyle: "normal",
+  fontWeight: "bold",
+  padding: 1,
+  rotations: 3,
+  rotationAngles: [0, 90],
+  scale: "sqrt",
+  spiral: "archimedean",
+  transitionDuration: 1000
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Configure options in the code editor!</p>
+      <div style={{ height: 400, width: 600 }}>
+        <ReactWordcloud options={options} words={words} />
+      </div>
     </div>
   );
 }
---
+++ src/data/nagativeWords.js
@@ -0,0 +1,8 @@
+export default [
+    { text: "최악", value: 184 },
+    { text: "분실", value: 150 },
+    { text: "환불", value: 63 },
+    { text: "삭제", value: 61 },
+    { text: "손상", value: 62 },
+    { text: "고생", value: 53 },    
+]
